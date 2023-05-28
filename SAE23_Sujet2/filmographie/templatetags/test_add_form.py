441eefdc554158824a46ccfe0899451420464fb8
TheSolobaid <thesolobaid@gmail.com>
2023-05-28 19:12:40 +0200

    test_forms

--- SAE23_Sujet2/filmographie/templatetags/test_add_form.py
+++ SAE23_Sujet2/filmographie/templatetags/test_add_form.py
@@ -2,6 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.filter
-def class_name(value):
+@register.filter        #dit a la suite de se comporter comme un filtre pour le template 
+def add_form(value):
     return value.__class__.__name__
