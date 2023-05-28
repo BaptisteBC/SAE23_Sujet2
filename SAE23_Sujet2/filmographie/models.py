638733f7051a1c3f4fd0b0f492b06eb360119aa9
TheSolobaid <thesolobaid@gmail.com>
2023-05-28 15:29:15 +0200

    data bases

--- SAE23_Sujet2/filmographie/models.py
+++ SAE23_Sujet2/filmographie/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class Categorie(models.Model):
+    nom = models.CharField(max_length=255)
+    descriptif = models.TextField()
+
+class Film(models.Model):
+    titre = models.CharField(max_length=255)
+    annee_sortie = models.IntegerField()
+    affiche = models.BinaryField(null=True)  # longblob translates to BinaryField in Django
+    realisateur = models.CharField(max_length=255)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)  # foreign key to Categorie
+
+class Acteur(models.Model):
+    nom = models.CharField(max_length=255)
+    prenom = models.CharField(max_length=255)
+    age = models.IntegerField()
+    photos = models.BinaryField(null=True)
+
+class FilmActeur(models.Model):
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
+    acteur = models.ForeignKey(Acteur, on_delete=models.CASCADE)
+
+class Personne(models.Model):
+    PROFESSIONNEL = 'professionnel'
+    AMATEUR = 'amateur'
+    TYPE_CHOICES = [
+        (PROFESSIONNEL, 'Professionnel'),
+        (AMATEUR, 'Amateur'),
+    ]
+
+    pseudo = models.CharField(max_length=255)
+    nom = models.CharField(max_length=255)
+    prenom = models.CharField(max_length=255)
+    mail = models.EmailField(max_length=255)
+    mot_de_passe = models.CharField(max_length=255)  # consider using Django's built-in auth system instead
+    type = models.CharField(max_length=13, choices=TYPE_CHOICES)
+
+class Commentaire(models.Model):
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
+    personne = models.ForeignKey(Personne, on_delete=models.CASCADE)
+    note = models.IntegerField()
+    commentaire = models.TextField()
+    date = models.DateField()
